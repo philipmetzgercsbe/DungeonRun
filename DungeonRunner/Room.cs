ad83c1fa7edfbcc394604be2cf3502cc4e8eae06
samuel.gast <hoechstesetablissement@gmail.com>
2018-09-14 11:53:13 +0200

    4 Variables missing. Defined Getter and Setters.

--- DungeonRunner/Room.cs
+++ DungeonRunner/Room.cs
@@ -2,6 +2,36 @@ namespace DungeonRunner
 {
     public class Room
     {
-        
+        /*Monster Monster*/
+        /*Character Character*/
+        /*Trader trader*/
+        /*Random random*/
+        private int Round;
+        private int Level;
+        private int StatsAfterVictory;
+
+        public Room(int level, int statsAfterVictory)
+        {
+            Level = level;
+            StatsAfterVictory = statsAfterVictory;
+        }
+
+        public int Round1
+        {
+            get => Round;
+            set => Round = value;
+        }
+
+        public int Level1
+        {
+            get => Level;
+            set => Level = value;
+        }
+
+        public int StatsAfterVictory1
+        {
+            get => StatsAfterVictory;
+            set => StatsAfterVictory = value;
+        }
     }
 }
