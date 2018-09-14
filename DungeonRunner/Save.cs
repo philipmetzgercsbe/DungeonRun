04a8c2863c156a1c63cd4f153bfe2e265cf98a53
samuel.gast <hoechstesetablissement@gmail.com>
2018-09-14 17:14:34 +0200

    Little sketch.

--- DungeonRunner/Save.cs
+++ DungeonRunner/Save.cs
@@ -1,7 +1,26 @@
+using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
 namespace DungeonRunner
 {
+    
+    
     public class Save
     {
         
+        
+        public void takechar(ref Character[] Person1)
+        {
+            StreamWriter FileWriter = new StreamWriter("test.txt");
+            FileWriter.WriteLine(Person1.Length + 1);
+            
+            for (int i = 0; i < Person1.Length; i++)
+            {
+                
+                
+            }
+            
+        }
     }
 }
