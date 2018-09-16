c36df3e1071527dc228779b851d6e2bdbc6ae9e4
samuel.gast <hoechstesetablissement@gmail.com>
2018-09-16 16:30:44 +0200

    Functionalities in load.

--- DungeonRunner/Load.cs
+++ DungeonRunner/Load.cs
@@ -2,6 +2,54 @@ namespace DungeonRunner
 {
     public class Load
     {
+        private string pickchar;
         
+        public Load()
+        {
+           
+        }
+
+        public void LoadCharacter(/*ref CreateChar ch1*/)
+        {
+            using (StreamReader reader = new StreamReader("data.txt"))
+            {
+                Console.WriteLine("Here is a list of characters:");//show only names
+                string s;
+                do
+                {
+                    s = reader.ReadLine();
+                    Console.WriteLine(s/*ch1.ChName*/);
+                } while (s != null);
+
+            }
+        }
+
+        public void CheckIfCharExists()
+        {
+            Console.WriteLine("Pick a character");
+            pickchar = Console.ReadLine();
+            using (StreamReader sr = File.OpenText("data.txt"))
+            {
+                string[] lines = File.ReadAllLines("data.txt");
+                bool isMatch = false;
+                for (int x = 0; x < lines.Length - 1; x++)
+                {
+                    if (pickchar == lines[x])
+                    {
+                        Console.WriteLine("Char found");
+                        sr.Close();
+                        isMatch = true;
+                        Menu GameStart = new Menu();
+                        GameStart.GameMenu();
+                    }
+                }
+
+                if (!isMatch)
+                {
+                    Console.WriteLine("Char not found");
+                    sr.Close();
+                }
+            }
+        }
     }
 }
